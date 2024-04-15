58ede05b7fd18bb60c33941255d3cb1478cdb6fd
Jamin M <jaminmenter@outlook.com>
2024-04-14 17:09:27 -0700

    finish wrapping RPC methods

--- AquadoggoClient.ts
+++ AquadoggoClient.ts
@@ -2,6 +2,11 @@ import { loadPackageDefinition, ChannelCredentials } from "@grpc/grpc-js";
 import { load as loadProto } from '@grpc/proto-loader';
 import type { ProtoGrpcType } from "./rpc";
 import type { ConnectClient } from "./rpc/Connect";
+import type { DocumentRequest } from "./rpc/DocumentRequest";
+import type { CollectionRequest } from "./rpc/CollectionRequest";
+import { KeyPair, encodeOperation, signAndEncodeEntry, type EntryArgs, type OperationArgs } from "p2panda-js";
+
+const HASH_LEN = 68;
 
 export interface ClientOptions {
   serverUrl: string;
@@ -23,14 +28,49 @@ export class AquadoggoClient {
     this.grpcClient.close();
   }
 
-  async getDocument(documentId: string) {
+  async getCollection({ schemaId, pagination, order, filter }: CollectionRequest) {
+    if (!schemaId || schemaId.length < HASH_LEN) {
+      throw new Error('Missing or malformed schema ID');
+    }
+
     return new Promise((resolve, reject) => {
-      const call = this.grpcClient.getDocument({ documentId },
+      this.grpcClient.getCollection({ schemaId, pagination, order, filter },
+        (err, coll) => {
+          err && reject(err);
+          coll && resolve(coll);
+        }
+      )
+    });
+  }
+
+  async getDocument({ documentId, documentViewId }: DocumentRequest) {
+    if ((!documentId || documentId.length < HASH_LEN) ||
+        (!documentViewId || documentViewId.length < HASH_LEN)
+    ) {
+      throw new Error('Missing or malformed document ID and/or document view ID');
+    }
+
+    return new Promise((resolve, reject) => {
+      this.grpcClient.getDocument({ documentId, documentViewId },
         (err, doc) => {
           err && reject(err);
           doc && resolve(doc);
         }
       );
     });
+  }
+
+  async doPublish({ operationArgs, entryArgs, keyPair }: { operationArgs: OperationArgs, entryArgs: EntryArgs, keyPair: KeyPair }) {
+    const entry = signAndEncodeEntry(entryArgs, keyPair);
+    const operation = encodeOperation(operationArgs);
+
+    return new Promise((resolve, reject) => {
+      this.grpcClient.doPublish({ entry, operation },
+        (err, nextArgs) => {
+          err && reject(err);
+          nextArgs && resolve(nextArgs);
+        }
+      );
+    })
   }
 }
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ async function main() {
     serverUrl: 'localhost:2021' // omit 'http'
   });
 
-  const doc = await client.getDocument('0020a5c1c397b6f515f5df7f80c4d821fd0a70f098b9b3c3c1a06934ad638eea87aa');
+  const doc = await client.getDocument({ documentId: '0020a5c1c397b6f515f5df7f80c4d821fd0a70f098b9b3c3c1a06934ad638eea87aa' });
   console.log('DOC', doc);
   client.dispose();
 }
