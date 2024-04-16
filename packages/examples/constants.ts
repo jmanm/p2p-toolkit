461d4ceaf5a282d6860a6e10c5f63159d07b799b
Jamin M <jaminmenter@outlook.com>
2024-04-16 08:26:14 -0700

    remove schemas file; add fishy schema

--- packages/examples/constants.ts
+++ packages/examples/constants.ts
@@ -4,5 +4,5 @@ export const AQUADOGGO_GQL_ENDPOINT = 'http://0.0.0.0:2020/graphql';
 export const AQUADOGGO_RPC_ENDPOINT = 'http://localhost:2021';
 
 // Run `npm run schema` and paste the resulting schema ids into `schemas.json`
-export const USER_SCHEMA_ID = schemas.USER_SCHEMA_ID;
-export const ORGANIZATION_SCHEMA_ID = schemas.ORGANIZATION_SCHEMA_ID;
+export const USER_SCHEMA_ID = schemas.user;
+export const ORGANIZATION_SCHEMA_ID = schemas.organization;
