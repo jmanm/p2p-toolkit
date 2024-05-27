7685a20f31004726edec85bfe5b488781f8db875
Jamin M <jaminmenter@outlook.com>
2024-05-26 17:51:25 -0700

    make proto file part of the project

--- packages/toolkit/AquadoggoClient.ts
+++ packages/toolkit/AquadoggoClient.ts
@@ -13,7 +13,7 @@ const HASH_LEN = 68;
 
 export interface ClientOptions {
   serverUrl: string;
-  protoFilePath: string;
+  protoFilePath?: string;
   credentials: ChannelCredentials;
 }
 
@@ -21,7 +21,7 @@ export class AquadoggoClient {
   protected constructor(private grpcClient: ConnectClient) { }
 
   static async load(options: ClientOptions) {
-    const def = await loadProto(options.protoFilePath);
+    const def = await loadProto(options.protoFilePath ?? './proto/rpc.proto');
     const { rpc } = loadPackageDefinition(def) as unknown as ProtoGrpcType;
     const client = new rpc.Connect(options.serverUrl, options.credentials);
     return new AquadoggoClient(client);
