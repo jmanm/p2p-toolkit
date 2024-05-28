a7cc1acf49855201769f60dbbc3a5433453a52a2
Jamin M <jaminmenter@outlook.com>
2024-05-27 20:22:21 -0700

    export `verifySignature`

--- packages/toolkit/index.ts
+++ packages/toolkit/index.ts
@@ -4,12 +4,14 @@ import type { DocumentRequest } from "./rpc/DocumentRequest";
 import type { CollectionRequest } from "./rpc/CollectionRequest";
 import type { CollectionResponse } from "./rpc/CollectionResponse";
 import type { Document } from "./rpc/Document";
-import { KeyPair } from "p2panda-js";
+import { KeyPair, verifySignature } from "p2panda-js";
 
 export {
   AquadoggoClient,
   ChannelCredentials,
   KeyPair,
+
+  verifySignature
 }
 
 export type {
